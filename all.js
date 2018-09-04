a5b1b99f1020f65b0930a760b95ea33e8d3825b6
Lars Bohm <larsbohm@gmail.com>
2018-09-04 15:01:29 +0200

    add tab functionality

--- all.js
+++ all.js
@@ -1,5 +1,8 @@
 var hamburger = document.getElementById('hamburger');
 var hamburgerNav = document.querySelector('.hamburger-nav');
+var tabs = document.querySelectorAll('.tab');
+var tabContents = document.querySelectorAll('.tab-content');
+
 
 hamburger.addEventListener('click', function(event) {
   if (hamburgerNav.style.display === "none" || hamburgerNav.style.display === "") {
@@ -7,4 +10,18 @@ hamburger.addEventListener('click', function(event) {
   } else {
     hamburgerNav.style.display = "none";
   }
+})
+
+tabs.forEach(function(item) {
+  item.addEventListener('click', function(event) {
+    var target = event.currentTarget.dataset.target;
+    tabContents.forEach(function(item) {
+      item.classList.add('hidden');
+    })
+    document.getElementById(target).classList.remove('hidden');
+    tabs.forEach(function(item) {
+      item.classList.remove('active');
+    })
+    event.currentTarget.classList.add('active');
+  })
 })
